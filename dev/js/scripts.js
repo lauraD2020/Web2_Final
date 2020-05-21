d951310f47cecdeb8310f51a7180453245e9c673
lauraD2020 <63317971+lauraD2020@users.noreply.github.com>
2020-05-21 15:57:19 -0400

    initial stuff

---
+++ dev/js/scripts.js
@@ -0,0 +1,40 @@
+
+var $burger = $("#burger-icon");
+var $nav = $('nav[data-nav]');
+var $navA = $('nav[data-nav] a');
+var isVisible = false;
+
+function mouseClick(){
+
+    console.log("click");
+    if(isVisible === false){
+        $nav.show();
+        isVisible = true;
+    }
+    else{
+        $nav.hide();
+        isVisible = false;
+    }
+}
+
+$burger.on( "click", mouseClick);
+// close the mobile menu when menu link is clicked
+$navA.on( "click", mouseClick);
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
