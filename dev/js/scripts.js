3cccce1e6c09b328571567b43ad96dbe2d93cad7
lauraD2020 <63317971+lauraD2020@users.noreply.github.com>
2020-06-01 22:29:14 -0400

    updates

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -38,3 +38,52 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";
 }
+
+
+$(document).ready(function() {
+
+  // $('.carousel').slick({
+  //     infinite: true,
+  //     slidesToShow: 1,
+  //     slidesToScroll: 1,
+  //     adaptiveHeight: true,
+  //     // autoplay: true,
+  //     fade: true,
+  //     cssEase: 'linear',
+  //     // arrows: false,
+  //     dots: true
+  // });
+  $('.carousel').slick({
+      dots: true,
+      arrows: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      mobileFirst: true,
+      adaptiveHeight: true,
+      responsive: [
+          {
+              breakpoint: 1200,
+              // settings: "unslick"
+          },
+          {
+              breakpoint: 992,
+              settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+              }
+          },
+          {
+              breakpoint: 768,
+              settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+              }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+      ]
+  });
+});
