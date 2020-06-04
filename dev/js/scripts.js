bee809165db98eee213e38ea854b5ed82333cd4a
lauraD2020 <63317971+lauraD2020@users.noreply.github.com>
2020-06-03 21:38:19 -0400

    big fixes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -21,24 +21,6 @@ $burger.on( "click", mouseClick);
 // close the mobile menu when menu link is clicked
 $navA.on( "click", mouseClick);
 
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-}
-
 
 $(document).ready(function() {
 
